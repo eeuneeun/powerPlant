873c51c9bbf7fe61b1c4045e8fbc01e6fb46d1ff
eeuneeun <sacroo89@gmail.com>
2021-04-27 10:22:35 +0900

    기본 데이터 추출 기능 완성!

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,80 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react';
 import { connectToDatabase } from '../util/mongodb';
-import mongoKeyEsc from 'mongo-key-escape';
+import Tabulator from "tabulator-tables";
+import { Button } from 'semantic-ui-react';
+import 'tabulator-tables/dist/js/tabulator.min.js';
+import "tabulator-tables/dist/css/tabulator.min.css"; 
+import 'semantic-ui-css/semantic.min.css';
 
-export default function Home({ properties }) {
+
+export default function Index(props) {
   
-  const [ data, setData ] = useState()
+  const data = props.data;
+  let genDT = null;
   
+  function setGenDT() {
+      genDT = new Tabulator("#table", {
+      layout: "fitDataStretch",
+      columns: [
+        { title: "사업소명", field: "branch", width: 150, editor:"input" },
+        { title: "행정구역", field: "district", width: 150, editor:"input" },
+        { title: "태양광명", field: "sunlight", sorter: "date", width: 150, editor:"input"},
+        // { title: "용량 MW", field: "amountsMW", width: 150, editor:"input" },
+        { title: "일시", field: "date", width: 150 },
+        { title: "발전량 kWh", field: "amountsKWH", width: 150 }
+      ],
+    });
+  }
+
+  function getGenData(){
+    const timeLine = [
+      "0:00",
+      "1:00",
+      "2:00",
+      "3:00",
+      "4:00",
+      "5:00",
+      "6:00",
+      "7:00",
+      "8:00",
+      "9:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00"
+    ]
+    let tmp = [];
+    data.map(item => {
+      item.amountsKWH.map((genAmounts, idx) =>{
+        console.log(item.data);
+        tmp.push({
+          branch : item.branch,
+          district : item.district,
+          sunlight : item.sunlight,
+          date: item.date + " " + timeLine[idx],
+          amountsKWH: genAmounts
+        });
+      });     
+    })
+    
+    genDT.replaceData(tmp);
+
+  }
+
   useEffect(() => {
-    setData(properties);
-    console.log(data);
+    setGenDT();
+    getGenData();
   }, []);
 
   return (
@@ -18,48 +83,71 @@ export default function Home({ properties }) {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src="https://oss.sheetjs.com/sheetjs/xlsx.full.min.js"></script>
       </Head>
       <div>
-        <h1>으아아~~</h1>
-        {data && data.map((item)=>(
-          <ul key={item.id}>
-            <li>{item.branch}</li>
-            <li>{item.district}</li>
-            <li>{item.sunlight}</li>
-            <li>{item.amount}</li>
-          </ul>
-        ))}
+        <h1>발전소 데이터</h1>
+       
+        <Button color='green'>엑셀 다운로드</Button>
+        <div id="table"></div>
       </div>
     </div>
     </>
   )
 }
 
+
 export async function getServerSideProps(context) {
   const { db } = await connectToDatabase();
-  const data = await db.collection("generation").find({}).limit(20).toArray();
-  const data2 = await db.collection("generation").aggregate({
-    $match : {
-      "0:00" : "<value>"
-    }
-  });
+  const rawData = await db.collection("generation").find({}).limit(20).toArray();
+  // const data2 = await db.collection("generation").aggregate({
+  //   $match : {
+  //     "0:00" : "<value>"
+  //   }
+  // });
   
-  console.log(data2)
+  // console.log(data2)
 
-  const properties = JSON.parse(JSON.stringify(data));
-  const filtered  = properties.map((item)=>{
+  const parsingData = JSON.parse(JSON.stringify(rawData));
+  const filteredData  = parsingData.map((item)=>{
     return{
-      ...properties,
       id : item._id,
       branch : "한국남동발전",
       district : item.행정구역,
       sunlight : item.태양광명,
-      date : item.년월일
+      // amountsKWH : item.배터리용량,  //원천 데이터 파일에 값이 있을 수도 있고, 없을 수도 있어서 일단 제외
+      date : item.년월일,
+      amountsKWH : [
+        item.시간0,
+        item.시간1,
+        item.시간2,
+        item.시간3,
+        item.시간4,
+        item.시간5,
+        item.시간6,
+        item.시간7,
+        item.시간8,
+        item.시간9,
+        item.시간10,
+        item.시간11,
+        item.시간12,
+        item.시간13,
+        item.시간14,
+        item.시간15,
+        item.시간16,
+        item.시간17,
+        item.시간18,
+        item.시간19,
+        item.시간20,
+        item.시간21,
+        item.시간22,
+        item.시간23
+      ]
+
     }
   })
-  console.log(properties)
 
   return {
-    props: { properties:properties },
+    props: { data:filteredData },
   }
 }
